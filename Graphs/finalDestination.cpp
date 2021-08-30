c34d5f2142210f5303fe5b8322f7a96ae03d5ed0
SHUBHAM KHARE <69118006+khare1872@users.noreply.github.com>
2021-08-30 09:10:57 +0530

    Create finalDestination.cpp

---
+++ Graphs/finalDestination.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int canReach(long long a, long long b, long long x) {
+        int minimumStepsRequired = abs(a)+abs(b);
+        if(minimumStepsRequired>x) return 0;
+        if((x-minimumStepsRequired)%2==0) return 1;
+        return 0;
+    }
+};
